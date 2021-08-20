69935caf30a3e917dae140843dc77d3ea4e911b5
shuta13 <hollystarsun@gmail.com>
2021-08-21 00:56:05 +0900

    wip: `render` 関数の実装

--- packages/toy-react-dom/src/client/ToyReactDOM.js
+++ packages/toy-react-dom/src/client/ToyReactDOM.js
@@ -1,5 +1,73 @@
+// @ts-check
 'use strict';
 
+import {
+  updateContainer,
+  flushSyncWithoutWarningIfAlreadyRendering,
+  getPublicRootInstance,
+} from 'toy-react-reconciler';
+
+function createRootFromDOMContainer(container, forceHydrate) {}
+
+/**
+ * @param {*} parentComponent
+ * @param {*} children
+ * @param {*} container
+ * @param {boolean} forceHydrate
+ * @param {Function=} callback
+ * @returns
+ */
+function renderSubtreeIntoContainer(
+  parentComponent,
+  children,
+  container,
+  forceHydrate,
+  callback
+) {
+  let root = container._toyReactRootContainer;
+  let fiberRoot;
+
+  if (!root) {
+    root = container._toyReactRootContainer = createRootFromDOMContainer(
+      container,
+      forceHydrate
+    );
+    fiberRoot = root;
+
+    if (typeof callback === 'function') {
+      const originalCallback = callback;
+      callback = function () {
+        const instance = getPublicRootInstance(fiberRoot);
+        originalCallback.call(instance);
+      };
+    }
+
+    flushSyncWithoutWarningIfAlreadyRendering(() => {
+      updateContainer(children, fiberRoot, parentComponent, callback);
+    });
+  } else {
+    fiberRoot = root;
+
+    if (typeof callback === 'function') {
+      const originalCallback = callback;
+      callback = function () {
+        const instance = getPublicRootInstance(fiberRoot);
+        originalCallback.call(instance);
+      };
+    }
+
+    updateContainer(children, fiberRoot, parentComponent, callback);
+  }
+
+  return getPublicRootInstance(fiberRoot);
+}
+
+/**
+ * @param {*} element
+ * @param {*} container
+ * @param {Function=} callback
+ * @returns
+ */
 export function render(element, container, callback) {
-  console.log('This is render.');
+  return renderSubtreeIntoContainer(null, element, container, false, callback);
 }
