f79cf6aef4aa3593b05c744a8c7a4daef4e05562
shuta13 <hollystarsun@gmail.com>
2021-08-21 22:59:09 +0900

    feat: fix all errors

--- packages/toy-react-reconciler/src/ToyReactFiberReconciler.js
+++ packages/toy-react-reconciler/src/ToyReactFiberReconciler.js
@@ -1,1 +1,12 @@
 'use strict';
+
+export function updateContainer(
+  element,
+  container,
+  parentComponent,
+  callback
+) {}
+
+export { flushSyncWithoutWarningIfAlreadyRendering } from './ToyReactFiberWorkLoop';
+
+export function getPublicRootInstance(container) {}
