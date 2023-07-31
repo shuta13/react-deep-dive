86bb64e9e89ec7ad47ad7b299584d0235bb82a96
shuta13 <hollystarsun@gmail.com>
2023-07-31 09:12:26 +0900

    chore: init

--- fixtures/ssg-app/src/generate.js
+++ fixtures/ssg-app/src/generate.js
@@ -1,38 +1,3 @@
 'use strict';
-const path = require('path');
-const fs = require('fs');
-const ToyReact = require('toy-react');
-const ToyReactDOMServer = require('toy-react-dom/server');
-const { App } = require('./components/App');
 
-const routes = fs
-  .readdirSync(path.resolve(__dirname, './pages'), { withFileTypes: true })
-  .filter((dirent) => dirent.name.endsWith('.js'))
-  .map((dirent) => dirent.name.replace(/\.js$/, ''));
-
-(() => {
-  routes.forEach((route) => {
-    const content = ToyReactDOMServer.renderToString(<App />);
-    const template = fs.readFileSync(
-      path.resolve(__dirname, 'app.html'),
-      'utf-8'
-    );
-    const html = template.replace('<!-- app -->', content);
-    if (!fs.existsSync(path.resolve(__dirname, '../out'))) {
-      fs.mkdirSync(path.resolve(__dirname, '../out'));
-    }
-    const outPath = path.resolve(__dirname, `../out/${route}.html`);
-    fs.writeFileSync(outPath, html);
-    fs.copyFileSync(
-      path.resolve(__dirname, `./${route}.css`),
-      path.resolve(__dirname, `../out/${route}.css`)
-    );
-    if (!fs.existsSync(path.resolve(__dirname, '../out/pages'))) {
-      fs.mkdirSync(path.resolve(__dirname, '../out/pages'));
-    }
-    fs.copyFileSync(
-      path.resolve(__dirname, `./pages/${route}.js`),
-      path.resolve(__dirname, `../out/pages/${route}.js`)
-    );
-  });
-})();
+// TODO
--- fixtures/ssg-app/src/pages/index.js
+++ fixtures/ssg-app/src/pages/index.js
@@ -1,10 +1,3 @@
 'use strict';
 
-const ToyReact = require('toy-react');
-const ToyReactDOM = require('toy-react-dom');
-
-require('../assets/styles/global.css');
-
-const { App } = require('../components/App');
-
-ToyReactDOM.hydrate(<App />, document.getElementById('root'));
+// TODO
